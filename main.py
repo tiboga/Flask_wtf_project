d7d4561bfc60495154fb154660a32e35e8b69d4d
TIBO <bor_tim@list.ru>
2024-03-10 12:52:09 +0300

    second task not resolved

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ def index(title):
     return render_template('index.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, href_for_engineer=url_for('static', filename='img/engineer.png'),
+                           href_for_nonengineer=url_for('static', filename='img/nonengineer.png'))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
